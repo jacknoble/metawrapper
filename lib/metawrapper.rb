14e99d533f9b4e10dfaa005516379fd7f752fde2
jacknoble <jack.noble@gmail.com>
2014-04-17 04:34:20 -0700

    add attr_wrapper to define wrapping methods easily

--- lib/metawrapper.rb
+++ lib/metawrapper.rb
@@ -1,21 +1,58 @@
 module Metawrapper
 
-	def method_missing (method_name, *args, &blk)
-		variable_found = false
-		instance_variables.each do |var|
-			if variable_responds_to_method?(var, method_name)
-				variable_found = true
-				return instance_variable_get(var).send(method_name, *args, &blk)
+	def self.included(base)
+		base.send(:include, InstanceMethods)
+		base.extend(ClassMethods)
+	end
+
+	module InstanceMethods
+
+		def method_missing(method_name, *args, &blk)
+			variable_found = false
+			instance_variables.each do |var|
+				if variable_responds_to_method?(var, method_name)
+					variable_found = true
+					return instance_variable_get(var).send(method_name, *args, &blk)
+				end
+			end
+			super unless variable_found
+		end
+
+
+		def variable_responds_to_method?(var, method)
+			instance_variable_get(var).respond_to?(method)
+		end
+
+
+	end
+
+	module ClassMethods
+
+		def attr_wrapper(var_to_methods_hash)
+			wrapped_variables = var_to_methods_hash.keys
+			wrapped_variables.each do |var|
+				methods = var_to_methods_hash[var]
+				methods = unique_methods_for_variable(var) if methods == :every
+				add_methods(var, methods)
 			end
 		end
-		super unless variable_found
+
+		def unique_methods_for_variable_class(var)
+			variable = instance_variable_get(var)
+			variable.methods.select do |method|
+				!self.instance_methods.include?(method)
+			end
+		end
+
+
+		def add_methods(var, methods)
+			methods.each do |method|
+				define_method(method) do |*args, &prc|
+					instance_variable_get('@'+ var.to_s).send(method, *args, &prc)
+				end
+			end
+		end
+
 	end
-
-	def variable_responds_to_method?(var, method)
-		instance_variable_get(var).respond_to?(method)
-	end
-
-	
-
 
 end
--- test/metawrapper_test.rb
+++ test/metawrapper_test.rb
@@ -1,23 +1,25 @@
 require_relative '../lib/metawrapper.rb'
 class TestWithCollection
-	# include Metawrapper
-	include Enumerable
+	include Metawrapper
+	# include Enumerable
 	attr_reader :stuff
+	attr_wrapper :stuff => [:max, :min, :each], :other_stuff => [:[]]
 
 	def initialize
 		@stuff = [1,2,3,4,5, 6]
+		@other_stuff = {test: "hash is working"}
 	end
 
-	def each(&prc)
-		@stuff.each(&prc)
-	end
+	# def each(&prc)
+	# 	@stuff.each(&prc)
+	# end
 
 end
 
 t = TestWithCollection.new
-# t.each do |el|
-# 	puts el
-# end
+t.each do |el|
+	puts el
+end
 
-t[3]
-
+# puts t
+print t
